4f3a275eca09f43bb7b3480e494538260d2abb78
Nicola Di Candia <nicoladicandiajaimes@gmail.com>
2019-11-12 16:54:50 -0500

    progress Nov 12

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
-//READ DATA FROM DB 
-include_once 'conections.php'; //cargo la conexion
+//READ DATA FROM DB
+include_once 'conection.php'; //cargo la conexion
 $sql_reader = 'SELECT * FROM users'; //var con consulta sql
 $gsent = $pdo->prepare($sql_reader); //guardo la consulta en una var
 $gsent->execute(); //ejecuto la consulta
@@ -8,25 +8,29 @@ $result = $gsent->fetchAll(); //guardo la consulta en un array
 //var_dump($result); //muestro el array
 
 //ADD DATA IN DB
-    if ($_POST) {
-        $name = $_POST['name'];
-        $document = $_POST['document'];
-        $email = $_POST['email'];
+if (!empty($_POST['name']) && !empty($_POST['document']) && !empty($_POST['email'])) {
+    $name = $_POST['name'];
+    $document = $_POST['document'];
+    $email = $_POST['email'];
 
-        $sql_add = 'INSERT INTO users (name,document,email) VALUES (?,?,?) ';
-        $sentencia_agregar = $pdo->prepare($sql_add);
-        $sentencia_agregar->execute(array($name, $document, $email));
-        header('location:index.php');
-    }else{
-        
-    }
-?> 
+    $sql_add = 'INSERT INTO users (name,document,email) VALUES (?,?,?) ';
+    $sentence_add = $pdo->prepare($sql_add);
+    $sentence_add->execute(array($name, $document, $email));
+
+    header('location:index.php');
+} else {
+    //modal (no data to regist) JS  (pending) 
+}
+
+//EDIT DATA IN DB
+
+
+?>
 <!doctype html>
 <html lang="en">
 
 <head>
     <div class="bootstrap">
-
         <!-- Required meta tags -->
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -36,7 +40,7 @@ $result = $gsent->fetchAll(); //guardo la consulta en un array
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
         <!-- navbar bootstrap-->
-        <nav class="navbar navbar-dark bg-light">
+        <nav class="navbar navbar-light bg-light">
             <a class="navbar-brand">
                 <img src="util/mainlogoW.svg" width="50%" height="50%" class="d-inline-block align-top" alt="">
             </a>
@@ -63,6 +67,9 @@ $result = $gsent->fetchAll(); //guardo la consulta en un array
                         <?php echo $data['name']; ?>
                         -
                         <?php echo $data['document']; ?>
+                        <a href="" class="float-right">
+                            <button>edit</button>
+                        </a>
                     </div>
                 <?php endforeach ?>
             </div>
@@ -75,7 +82,6 @@ $result = $gsent->fetchAll(); //guardo la consulta en un array
                     <input type="text" class="form-control mt-3" name="document" placeholder="Document">
                     <input type="text" class="form-control mt-3" name="email" placeholder="Email">
                     <button class="btn btn-success mt-4">Add</button>
-                    <button class="btn btn-dark mt-4 ml-3">Clean</button>
                 </form>
             </div>
         </div>
@@ -84,9 +90,12 @@ $result = $gsent->fetchAll(); //guardo la consulta en un array
     <div class="javascript-adds">
         <!-- Optional JavaScript -->
         <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+        </script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
+        </script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
+        </script>
     </div>
 
 </body>
