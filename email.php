589a32d2165eddcd171f83c5dd99eafbe6c45e89
Nicola Di Candia <nicoladicandiajaimes@gmail.com>
2019-11-24 19:20:40 -0500

    no password

--- email.php
+++ email.php
@@ -56,7 +56,7 @@ try {
   $mail->Host       = 'smtp.gmail.com';                       // Set the SMTP server to send through
   $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
   $mail->Username   = 'nicoladicandiajaimes@gmail.com';       // SMTP username
-  $mail->Password   = 'nicox2104';                            // SMTP password
+  $mail->Password   = '*';                            // SMTP password
   $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption
   $mail->Port       = 587;                                    // TCP port to connect to
 
